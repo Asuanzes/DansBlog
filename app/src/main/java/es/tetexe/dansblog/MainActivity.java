4669d9f6d2ee40b1637075723115d7cd7184b369
Shochlves <coco1970@gmail.com>
2015-09-02 20:09:59 +0200

    Primer commit en personal

--- app/src/main/java/es/tetexe/dansblog/MainActivity.java
+++ app/src/main/java/es/tetexe/dansblog/MainActivity.java
@@ -1,7 +1,8 @@
 package es.tetexe.dansblog;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -33,5 +34,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            return " ";
+        }
     }
 }
