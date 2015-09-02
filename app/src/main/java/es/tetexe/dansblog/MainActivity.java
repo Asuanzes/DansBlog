d76f4d58a91f110af25ed4687238b7a5c6d265b8
Shochlves <coco1970@gmail.com>
2015-09-02 21:06:39 +0200

    Update

--- app/src/main/java/es/tetexe/dansblog/MainActivity.java
+++ app/src/main/java/es/tetexe/dansblog/MainActivity.java
@@ -3,15 +3,42 @@ package es.tetexe.dansblog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btnParse;
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnParse = (Button) findViewById(R.id.btnParse);
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
+
+        btnParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        new DownloadData().execute("http://www.enriquedans.com/feed");
     }
 
     @Override
@@ -38,9 +65,72 @@ public class MainActivity extends AppCompatActivity {
 
     private class DownloadData extends AsyncTask<String, Void, String> {
 
+        String myXmlData;
+
+
         @Override
         protected String doInBackground(String... urls) {
+            try {
+
+                myXmlData = downloadXML(urls[0]);
+
+            } catch (IOException e) {
+                return "Unable to download XML file.";
+            }
             return null;
         }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("OnpostExecute", myXmlData);
+
+        }
+
+        private String downloadXML(String theUrl) throws IOException {
+
+            int BUFFER_SIZE = 2000000;
+
+            InputStream is = null;
+
+            String xmlContents = "";
+
+            try {
+                URL url = new URL(theUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(20000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                int response = conn.getResponseCode();
+                Log.d("DownloadXMl", "The response returned is:" + response);
+                is = conn.getInputStream();
+
+                InputStreamReader isr = new InputStreamReader(is);
+
+                int charRead;
+
+                char[] inputBuffer = new char[BUFFER_SIZE];
+                try {
+                    while ((charRead = isr.read(inputBuffer)) > 0) {
+                        String readString = String.copyValueOf(inputBuffer, 0, charRead);
+                        xmlContents += readString;
+                        inputBuffer = new char[BUFFER_SIZE];
+                    }
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+
+            } finally {
+                if (is != null)
+                    is.close();
+            }
+            //Colocado en un lugar diferente al vídeo
+            return xmlContents;
+
+        }
+
     }
 }
